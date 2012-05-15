ec59c2cf87604d1196501e9f250f503f272523a3
tazro inutano ohta <inutano@gmail.com>
2012-05-15 19:40:52 +0900

    fully refactorred

--- ohayau.rb
+++ ohayau.rb
@@ -1,32 +1,56 @@
-#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
 require "yaml"
 require "twitter"
+require "ap"
 
-y_config = YAML.load_file("/Users/iNut/ohayau/ohayauconfig.yaml")
-
-Twitter.configure do |config|
-	config.consumer_key = y_config["twitter"]["consumer_key"]
-	config.consumer_secret = y_config["twitter"]["consumer_secret"]
-	config.oauth_token = y_config["twitter"]["oauth_token"]
-	config.oauth_token_secret = y_config["twitter"]["oauth_token_secret"]
+def randomize(neta)
+  neta.scan(/./).shuffle.join
 end
 
-tw = Twitter::Client.new
+if __FILE__ == $0
+  current = File.expand_path(File.dirname(__FILE__))
+  y_conf = YAML.load_file("#{current}/ohayauconfig.yaml")
+  tw_conf = y_conf[:twitter]
+  
+  Twitter.configure do |config|
+    config.consumer_key = tw_conf[:consumer_key]
+    config.consumer_secret = tw_conf[:consumer_secret]
+    config.oauth_token = tw_conf[:oauth_token]
+    config.oauth_token_secret = tw_conf[:oauth_token_secret]
+  end
+  
+  tw = Twitter::Client.new
+  arg = ARGV.first
+  
+  case
+  when arg == "--post"
+    text = ARGV[1]
+    tw.update(text)
 
-if ARGV[0] == "-p"
-	tw.update(ARGV[1])
-
-elsif ARGV[0] == "--ohayau"
-	ohayaubullet = open("/Users/iNut/ohayau/ohayau.txt").readlines.sample
-	tw.update(ohayaubullet)
-
-elsif ARGV[0] == "--beer"
-	beer = "#びゐる"
-	space = ""
-	rand(17).times do
-		space += "　"
-		tw.update("#{beer}#{space}")
-	end
+  when arg == "--beer"
+    beer = y_config[:beer]
+    ws = ""
+    rand(17).times do
+      text = beer + ws
+      tw.update(text)
+      ws += y_conf[:whitespace]
+    end
+    
+  when arg == "--ohayau"
+    ws = ""
+    begin
+      neta = y_conf[:ohayau].sample
+      text = neta + ws
+      tw.update(text)
+    rescue
+      ws += y_conf[:whitespace]
+      retry
+    end
+    
+  when arg == "--randomize"
+    neta = ARGV[1]
+    text = randomize(neta)
+    tw.update(text)
+  end
 end
