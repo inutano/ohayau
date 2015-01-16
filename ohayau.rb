f1242e1553371d22cdecabdf1960d9ff7c7c293f
tazro inutano ohta <inutano@gmail.com>
2015-01-16 23:17:30 +0900

    added several features

--- ohayau.rb
+++ ohayau.rb
@@ -2,6 +2,8 @@
 
 require "yaml"
 require "twitter"
+require "user_stream"
+require "open-uri"
 require "ap"
 
 def randomize(neta)
@@ -19,25 +21,49 @@ if __FILE__ == $0
     config.oauth_token = tw_conf[:oauth_token]
     config.oauth_token_secret = tw_conf[:oauth_token_secret]
   end
+
+  UserStream.configure do |config|
+    config.consumer_key = tw_conf[:consumer_key]
+    config.consumer_secret = tw_conf[:consumer_secret]
+    config.oauth_token = tw_conf[:oauth_token]
+    config.oauth_token_secret = tw_conf[:oauth_token_secret]
+  end
   
   tw = Twitter::Client.new
   arg = ARGV.first
   
-  case
-  when arg == "--post"
+  us = UserStream.client
+  
+  case arg
+  when "--post"
     text = ARGV[1]
     tw.update(text)
 
-  when arg == "--beer"
+  when "--beer"
     beer = y_conf[:beer]
     ws = ""
-    rand(17).times do
+    rand(23).times do
+      text = beer + ws
+      tw.update(text)
+      ws += y_conf[:whitespace]
+    end
+
+  when "--rain-of-beer"
+    beer = y_conf[:beer]
+    rand(23).times do
+      num_of_beer = rand(14)
+      text = beer * num_of_beer
+      tw.update(text)
+    end
+    
+    ws = ""
+    rand(23).times do
       text = beer + ws
       tw.update(text)
       ws += y_conf[:whitespace]
     end
     
-  when arg == "--ohayau"
+  when "--ohayau"
     ws = ""
     begin
       neta = y_conf[:ohayau].sample
@@ -48,9 +74,45 @@ if __FILE__ == $0
       retry
     end
     
-  when arg == "--randomize"
+  when "--randomize"
     neta = ARGV[1]
     text = randomize(neta)
     tw.update(text)
+    
+  when "--repeat"
+    count = ARGV[1]
+    neta = ARGV[2]
+    ws = ""
+    count.to_i.times do
+      text = neta + ws
+      tw.update(text)
+      ws += y_conf[:whitespace]
+    end
+
+  when "--us-test"
+    us.sample do |status|
+      unless status["friends"]
+        ap status.user.name
+        ap status.text
+      end
+    end
+
+  when "--server-monitor"
+    ip = ARGV[1]
+    loop do
+      status = system("ping -c 1 #{ip} > /dev/null")
+      if status
+        tw.update("@iNut りなちゃん起きたよ！")
+        break
+      else
+        puts "she's still sleeping.. #{Time.now}"
+        sleep 10
+      end
+    end
+  
+  when "--dbclswebsite-monitor"
+    url = "http://dbcls.rois.ac.jp"
+    page = open(url).read
+    #tw.update("@iNut 死んだ") if page !~ /<title>Database/
   end
 end
